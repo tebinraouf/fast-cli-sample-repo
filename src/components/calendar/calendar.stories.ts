0775a4967c29c661c33b3b3ad330eff595a976d4
Tebin <tebin.raouf@gmail.com>
2022-08-27 14:56:48 -0400

    update calendar and tooltip

--- src/components/calendar/calendar.stories.ts
+++ src/components/calendar/calendar.stories.ts
@@ -1,10 +1,30 @@
 import Template from "./fixtures/base.html";
-import { DesignSystem } from "@microsoft/fast-foundation";
+import { DesignSystem, DataGrid, dataGridTemplate, DataGridCell, dataGridCellTemplate, DataGridRow, dataGridRowTemplate } from "@microsoft/fast-foundation";
 import { calendarDefinition } from "./define.js";
+
+
+const dataGridDefinition = DataGrid.compose({
+    baseName: "data-grid",
+    template: dataGridTemplate
+});
+
+const dataGridCellDefinition = DataGridCell.compose({
+    baseName: "data-grid-cell",
+    template: dataGridCellTemplate
+});
+
+const dataGridRowDefinition = DataGridRow.compose({
+    baseName: "data-grid-row",
+    template: dataGridRowTemplate
+});
+
 
 DesignSystem.getOrCreate().withPrefix(
     "saf"
 ).register(
+    dataGridCellDefinition(),
+    dataGridRowDefinition(),
+    dataGridDefinition(),
     calendarDefinition()
 );
 
--- src/components/tooltip/tooltip.stories.ts
+++ src/components/tooltip/tooltip.stories.ts
@@ -1,10 +1,18 @@
-import { DesignSystem } from "@microsoft/fast-foundation";
+import { AnchoredRegion, anchoredRegionTemplate, DesignSystem } from "@microsoft/fast-foundation";
 import Template from "./fixtures/base.html";
 import { tooltipDefinition } from "./define.js";
+
+
+const anchoredRegionDefinition = AnchoredRegion.compose({
+    baseName: 'anchored-region',
+    template: anchoredRegionTemplate
+});
+
 
 DesignSystem.getOrCreate().withPrefix(
     "saf"
 ).register(
+    anchoredRegionDefinition(),
     tooltipDefinition()
 );
 
